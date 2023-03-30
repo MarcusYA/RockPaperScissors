aee9dde5dd1369361665d93f1a5a9fca2d61811c
MarcusYA <marcus.78@hotmail.com>
2023-03-30 15:10:27 +0000

    added a click event in JS and also put a class of button to the buttons, also checked if I was able to get the text of the buttons. It worked!

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,3 +2,10 @@ const oponentChoiceDisplay = document.getElementById('oponentChoiceDisplay');
 const yourChoiceDisplay = document.getElementById('yourChoiceDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
 const buttonPushed = document.querySelectorAll('.button');
+let userChoice
+
+buttonPushed.forEach(button => button.addEventListener('click', () => {
+    userChoice = button.textContent;
+    yourChoiceDisplay.innerHTML = userChoice;
+}));
+console.log(userChoice);
