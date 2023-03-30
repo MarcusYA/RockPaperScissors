7e4552281c4818bb97ec7f6639311e18ebd7c3e0
MarcusYA <marcus.78@hotmail.com>
2023-03-30 14:59:34 +0000

    also a buttonPushed to be able to get the text from the button that is pushed

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,3 +1,4 @@
 const oponentChoiceDisplay = document.getElementById('oponentChoiceDisplay');
 const yourChoiceDisplay = document.getElementById('yourChoiceDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const buttonPushed = document.querySelectorAll('.button');
