542d35152565889cdfb84be9186ec5ba47ef7df7
MarcusYA <marcus.78@hotmail.com>
2023-03-31 00:11:50 +0000

    added some semicolon that was missing and also defined variables

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -2,127 +2,128 @@ const oponentChoiceDisplay = document.getElementById('oponentChoiceDisplay');
 const yourChoiceDisplay = document.getElementById('yourChoiceDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
 const buttonPushed = document.querySelectorAll('.btn');
-let userChoice
+let userChoice;
 const yourScore = document.getElementById("yourScore");
 const oponentScore = document.getElementById("oponentScore");
-let oponentChoice
+let oponentChoice;
+let result;
 
 buttonPushed.forEach(btn => btn.addEventListener('click', () => {
     userChoice = btn.textContent;
     yourChoiceDisplay.innerHTML = userChoice;
-    randomNumber()
-    resultShow()
-    reloadingPage()
-    reloadingPage2()
+    randomNumber();
+    resultShow();
+    reloadingPage();
+    reloadingPage2();
   
 }));
 
 function randomNumber() {
-    const randomNumber = Math.floor(Math.random() * 5) + 1 
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
     
     if (randomNumber === 1) {
-      oponentChoice = 'ROCK'
+      oponentChoice = 'ROCK';
     }
     if (randomNumber === 2) {
-      oponentChoice = 'SCISSORS'
+      oponentChoice = 'SCISSORS';
     }
     if (randomNumber === 3) {
-      oponentChoice = 'PAPER'
+      oponentChoice = 'PAPER';
     }
     if (randomNumber === 4) {
-        oponentChoice = 'LIZARD'
+        oponentChoice = 'LIZARD';
       }
       if (randomNumber === 5) {
-        oponentChoice = 'SPOCK'
+        oponentChoice = 'SPOCK';
       }
-    oponentChoiceDisplay.innerHTML = oponentChoice
+    oponentChoiceDisplay.innerHTML = oponentChoice;
   }
 
   function resultShow() {
     if (oponentChoice === userChoice) {
-      result = 'Draw!'
+      result = 'Draw!';
     }
     if (oponentChoice === 'ROCK' && userChoice === "PAPER") {
-      result = "Gratulations - You won!"
-      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1 
+      result = "Gratulations - You won!";
+      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
     }
     if (oponentChoice === 'SPOCK' && userChoice === "PAPER") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1 
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
     if (oponentChoice === 'PAPER' && userChoice === "LIZARD") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
     }
     if (oponentChoice === 'SPOCK' && userChoice === "LIZARD") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
     if (oponentChoice === 'SCISSORS' && userChoice === "SPOCK") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
     if (oponentChoice === 'ROCK' && userChoice === "SPOCK") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
     if (oponentChoice === 'LIZARD' && userChoice === "ROCK") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1  
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
     }
     if (oponentChoice === 'SCISSORS' && userChoice === "ROCK") {
-        result = "Gratulations - You won!"
-      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1 
+        result = "Gratulations - You won!";
+      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
     if (oponentChoice === 'LIZARD' && userChoice === "SCISSORS") {
-        result = "Gratulations - You won!"
-        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1
+        result = "Gratulations - You won!";
+        yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
     }
     if (oponentChoice === 'PAPER' && userChoice === "SCISSORS") {
-        result = "Gratulations - You won!"
-      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1 
+        result = "Gratulations - You won!";
+      yourScore.innerHTML = parseInt(yourScore.innerHTML)+1;
       }
       if (oponentChoice === 'PAPER' && userChoice === "ROCK") {
-        result = "Sorry - You lost!"
-          oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1 
+        result = "Sorry - You lost!";
+          oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
       }
       if (oponentChoice === 'PAPER' && userChoice === "SPOCK") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1 
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
       if (oponentChoice === 'LIZARD' && userChoice === "PAPER") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
       }
       if (oponentChoice === 'LIZARD' && userChoice === "SPOCK") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
       if (oponentChoice === 'SPOCK' && userChoice === "SCISSORS") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
       if (oponentChoice === 'SPOCK' && userChoice === "ROCK") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
       if (oponentChoice === 'ROCK' && userChoice === "LIZARD") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1  
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
       }
       if (oponentChoice === 'ROCK' && userChoice === "SCISSORS") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
       if (oponentChoice === 'SCISSORS' && userChoice === "LIZARD") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
       }
       if (oponentChoice === 'SCISSORS' && userChoice === "PAPER") {
-        result = "Sorry - You lost!"
-        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1
+        result = "Sorry - You lost!";
+        oponentScore.innerHTML = parseInt(oponentScore.innerHTML)+1;
         }
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
   }
 
   function reloadingPage() {
